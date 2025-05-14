93ccd0eb99086936f7d8fd0328286b52a73bb48e
Ital023 <contadoitalo911@gmail.com>
2025-05-14 01:31:44 -0300

    first commit

---
+++ src/test/java/io/github/ital023/freteflex/FreteflexApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.freteflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
